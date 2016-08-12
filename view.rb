b85155e03b31ebb3c8c6fb27c302008336952e08
Daniel Yoon & Joshua Marantz & Neel Gupta <pair+dyoon3102.jnaftali.neelgupta2@devbootcamp.com>
2016-08-12 11:31:15 -0400

    Add Viewer class, and slightly change syntax of passing the card front/back to the controller

---
+++ view.rb
@@ -0,0 +1,26 @@
+class FlashcardViewer
+  def display_greeting
+    puts "Hello! Welcome to Flashcards!"
+  end
+
+  def show_front(front_of_card)
+    puts "Question: #{front_of_card}"
+    gets.chomp
+  end
+
+  def right_answer
+    puts "Correct, well done!"
+  end
+
+  def wrong_answer
+    puts "Nope!"
+  end
+
+  def show_back(back_of_card)
+    puts back_of_card
+  end
+
+  def goodbye
+    puts "That's all I've got, goodbye!"
+  end
+end
