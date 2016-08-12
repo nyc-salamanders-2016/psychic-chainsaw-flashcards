aa4f278c2bb4fb8eae184226741f0a923b2470ab
Daniel Yoon & Joshua Marantz & Neel Gupta <pair+dyoon3102.jnaftali.neelgupta2@devbootcamp.com>
2016-08-12 11:04:49 -0400

    Basic controller and runner logic

---
+++ controller.rb
@@ -0,0 +1,24 @@
+require_relative 'parser'
+
+class FlashcardController
+  def initialize(path)
+    @path = path
+    @viewer = FlashcardViewer.new
+    @deck = Parser.parse_file(@path)
+    run
+  end
+
+  def run
+    @viewer.display_greeting
+    @deck.cards.each do |card|
+      response = @viewer.show_front(card)
+      if card.answer?(response)
+        @viewer.right_answer
+      else
+        @viewer.wrong_answer
+      end
+      @viewer.show_back(card)
+    end
+    @viewer.goobye
+  end
+end
